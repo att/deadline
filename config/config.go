85796ba5693d3d9f1d5f10680d9abc407ff4185e
Ohrstrom, Jeffrey G <jo424n@us.att.com>
2018-08-07 17:45:58 -0700

    a little formatting and moving testfiles to testdata dir

--- config/config.go
+++ config/config.go
@@ -2,20 +2,20 @@ package config
 
 import (
 	"errors"
-	"github.com/BurntSushi/toml"
-	"os"
 	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
 )
 
 var DefaultConfig = Config{
 	FileConfig: DefaultFileConfig,
-	Server:		DefaultServerConfig,
-	DAO:		"file",
+	Server:     DefaultServerConfig,
+	DAO:        "file",
 }
 
 var DefaultFileConfig = FileConfig{
 	Directory: ".",
-	
 }
 
 var DefaultDBConfig = DBConfig{
@@ -23,9 +23,7 @@ var DefaultDBConfig = DBConfig{
 	ConnectionString: "wearenotworkingwithdatabasesyet",
 }
 
-var DefaultEmailConfig = EmailConfig{
-
-}
+var DefaultEmailConfig = EmailConfig{}
 
 var DefaultServerConfig = ServConfig{
 	Port: "8081",
@@ -41,8 +39,6 @@ func validateConfig(c Config) error {
 	//db checks later
 }
 
-
-
 func LoadConfig(file string) (*Config, error) {
 
 	_, err := os.Stat(file)
@@ -54,26 +50,27 @@ func LoadConfig(file string) (*Config, error) {
 	if _, err := toml.DecodeFile(file, &conf); err != nil {
 		return &DefaultConfig, err
 	}
+
 	err = validateConfig(conf)
 	if err != nil {
-
 		return &DefaultConfig, err
 	}
-	if (conf.DAO == "DB") && (conf.DBConfig.ConnectionString == "") {
 
+	if (conf.DAO == "DB") && (conf.DBConfig.ConnectionString == "") {
 		fmt.Println("No DB specified.")
 		conf.DBConfig = DefaultDBConfig
 	}
-	if (conf.DAO == "file") {
-		if (conf.FileConfig.Directory == "") {
-		fmt.Println("No directory specified.")
-		conf.FileConfig = DefaultFileConfig
+
+	if conf.DAO == "file" {
+		if conf.FileConfig.Directory == "" {
+			fmt.Println("No directory specified.")
+			conf.FileConfig = DefaultFileConfig
 		}
 		if _, err := os.Stat(conf.FileConfig.Directory); os.IsNotExist(err) {
 			fmt.Println("Given directory doesn't exist.")
 			conf.FileConfig = DefaultFileConfig
 		}
-
 	}
+
 	return &conf, nil
 }
